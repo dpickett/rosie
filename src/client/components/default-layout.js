0285ac022b7d94e50543c472fd5e6c54d309d98b
Dan Pickett <dan.pickett@launchacademy.com>
2016-04-09 11:18:09 -0400

    improve aesthetics

--- src/client/components/default-layout.js
+++ src/client/components/default-layout.js
@@ -4,28 +4,36 @@ import { Link } from 'react-router';
 export default function DefaultLayout({children}){
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">Today</Link>
-        </li>
-        <li>
-          <Link to="/trello/Values">Values</Link>
-        </li>
-        <li>
-          <Link to="/trello/Annual Goals">Annual Goals</Link>
-        </li>
-        <li>
-          <Link to="/trello/Rocks">Rocks</Link>
-        </li>
-        <li>
-          <Link to="/trello/This%20Week">This Week</Link>
-        </li>
-        <li>
-          <Link to="/trello/Done">Done</Link>
-        </li>
-      </ul>
+      <div className="row">
+        <div className="column align-center">
+          <ul className="menu">
+            <li className="active">
+              <Link to="/">Today</Link>
+            </li>
+            <li>
+              <Link to="/trello/Values">Values</Link>
+            </li>
+            <li>
+              <Link to="/trello/Annual Goals">Annual Goals</Link>
+            </li>
+            <li>
+              <Link to="/trello/Rocks">Rocks</Link>
+            </li>
+            <li>
+              <Link to="/trello/This%20Week">This Week</Link>
+            </li>
+            <li>
+              <Link to="/trello/Done">Done</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-      {children}
+      <div className="row">
+        <div className="column align-center">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
