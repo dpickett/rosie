adc7c2627507d049f4e1f0a004acc2cb4d83ef8b
Dan Pickett <dan.pickett@launchacademy.com>
2016-04-26 17:42:54 -0400

    improve aesthetic of sign in prompt

--- src/client/components/sign-in-prompt.js
+++ src/client/components/sign-in-prompt.js
@@ -4,8 +4,13 @@ export default class SignInPrompt extends Component {
 
   render(){
     return (
-      <div>
-        <a className="button" href="/auth/github">Sign In with GitHub</a>
+      <div className="row">
+        <div className="column align-center">
+          <h1>Sign In</h1>
+          <a className="button" href="/auth/github">
+            <i className="fa fa-github fa-lg"></i> Sign In with GitHub
+          </a>
+        </div>
       </div>
     );
   }
